22bff3cf239160e392d9c925f00e2e6e742cb135
NicoleDanger <NicoleDanger@gmail.com>
2023-02-19 11:49:05 -0500

    Knowledge Check1 completed

--- KnowledgeCheck1.py
+++ KnowledgeCheck1.py
@@ -1,2 +1,20 @@
+## Repository for Knowledge Check 1
+
+## Part 1:  a statement that prints "hello world!"
 greetings = ("Hello, World!")
 print(greetings)
+
+## Part 2:  A list populated with several values. 
+##          Print a value from the list. 
+Groceries = ["milk", "butter", "tea", "water", "eggs"]
+print (Groceries[3])
+
+## Part 3: A dictionary populated with two keys and two values.
+##          Print one of the values.
+Prices = {"milk":3.99, "butter":4.99, "tea":2.99, "water":8.99, "eggs":3.59}
+print(Prices["milk"])
+
+## Part 4: A tuple with 4 values.
+##          Print one of them. 
+Non_Food_Items = ("soap", "alcohol", "cat food", "tampons", "gum")
+print(Non_Food_Items[3])
